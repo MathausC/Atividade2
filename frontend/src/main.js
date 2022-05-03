08124f5739d72bb56ea301d546d3bbf686591c99
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-03 09:52:32 -0300

    vuetify added to project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  vuetify,
   components: { App },
   template: '<App/>'
 })
